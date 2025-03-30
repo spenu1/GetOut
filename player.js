fd134e8374ad4689d10d0f0300ba0475224b2a8e
spenu1 <48360079+spenu1@users.noreply.github.com>
2025-03-30 19:47:44 +0100

    Update player.js

--- player.js
+++ player.js
@@ -6,10 +6,39 @@ export class Player{
         this.x = 0;
         this.y = 100;
         this.image = document.getElementById('player');
+        this.right = true
+        this.up = true
     }
 
     update(){
-        this.x++;
+        if(this.right == true){
+            this.x++
+        }
+        else{
+            this.x--
+        }
+
+        if(this.x <=0){
+            this.right = true;
+        }
+        if (this.x >= 1000){
+            this.right = false;
+        }
+
+        if(this.up == true){
+            this.y+=2
+        }
+        else{
+            this.y-=2
+        }
+
+        if(this.y <=0){
+            this.up = true;
+        }
+        if (this.y >= 1000){
+            this.up = false;
+        }
+
     }
 
     draw(context){
